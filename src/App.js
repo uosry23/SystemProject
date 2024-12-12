9b71c5a5486894c02fcc58908c58def1f15d8020
uosry23 <uosryahmed23@gmail.com>
2024-12-12 22:23:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import CardDesign from './CardDesign'
+const App = () => {
+  return (
+    <div>
+      <CardDesign />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/CardDesign.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Card.css'
+const CardDesign = () => {
+
+    return (
+        <div>
+            <div class="card">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT8d87diHjkqTnE1LOdVDSonAX5kAcIDqF_zGWm7_aKm4t6g9UjDxzirAi0bIa9zItkEk&usqp=CAU" alt="Card Image" />
+                <div class="details">
+                    <h2>tittle</h2>
+                    <p>This is a beautifully designed card with an image and some details. Customize it as you like!</p>
+                    <a href="#" class="button">Learn More</a>
+                </div>
+            </div></div>
+    )
+}
+
+export default CardDesign
