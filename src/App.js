ebd6da27e229441a81f694c92666c993ee696590
phoebealbier <120124630+phoebealbier@users.noreply.github.com>
2024-12-21 07:10:51 +0200

    created cart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./index.css";
 import CardDesign from "./CardDesign";
 import Profile from "./Profile";
-
+import Cart from './add-cart.js';
 const App = () => {
   return (
     <div>
       <Profile />
+      <Cart/>
     </div>
   );
 };
